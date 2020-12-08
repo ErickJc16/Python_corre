a559f0a923991c39434eb3f85cd1e167d8f85604
ErickJc16 <erick_itver@hotmail.com>
2020-12-08 12:31:30 -0600

    Agregue codigo de correlacion sin numpy

---
+++ sinNumpy.py
@@ -0,0 +1,31 @@
+#Algoritmo de correlacion sin utilizar Numpy 
+
+def zeros(z):
+    zeros_list = [0] * (z - 1)
+    return zeros_list
+
+def correlacion(x,h):
+    
+    #Arreglo de salida de la correlacion
+    rc = zeros(len(x) + len(h))
+
+    #Señal enviada con ceros agregados al inicio y al final
+    cx = zeros(len(h))
+    cx.extend(x)
+    cx.extend(zeros(len(h)))
+
+    #Ciclo for para calculo de valores
+    for n in range(len(rc)):
+        for j in range(len(h)):
+            idcx = n + j
+            rc[n] = rc[n] + h[j] * cx[idcx]
+
+    return rc 
+
+h = [1,5,-8]
+
+x = [3,2,1,6,-7]
+
+result = correlacion(x,h)
+
+print(result)
--- testcsv.py
+++ testcsv.py
@@ -6,13 +6,14 @@ result = []
 for i in range((len(x) + len(h)) - 1 ):
   print("-------")
   sample = 0
-  for j in range(0,-len(h),-1):
+  for j in range(1,len(h)+1):
+    print(j)
     if i - j < 0:
       continue
     if i - j > len(x) - 1:
       continue 
     sample += h[-j] * x[i-j]
-    print(sample)
+    #print(sample)
   result.insert(i,sample)
 
 print(result)
